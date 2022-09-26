7edb0c70766a1617ce045362b29e066ff6ec81d2
sahel <sahal.ea@gmail.com>
2022-09-26 08:13:43 +0400

    updated

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,9 +1,33 @@
-import useUsers from "hooks/user";
-import React from "react";
+import useUsers from 'hooks/user';
+import React from 'react';
 
 const Users = () => {
   const { usersList, pageConfig } = useUsers();
-  return <h1>Users list</h1>;
+  console.log(usersList);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersList.map((e) => (
+            <tr>
+              <td>{e.first_name}</td>
+              <td>{e.last_name}</td>
+              <td>{e.email}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Users;
